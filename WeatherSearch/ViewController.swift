e1e7eb15707da13f709ee7616aa00e88c9eaf59c
DevChaSsi <chassigood@gmail.com>
2022-11-08 15:30:49 +0900

    Initial Commit

---
+++ WeatherSearch/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  WeatherSearch
+//
+//  Created by SeonHo Cha on 2022/11/03.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    
+    @IBOutlet weak var cityNameTextField: UITextField!
+    @IBOutlet weak var temperatureLabel: UILabel!
+    @IBOutlet weak var humidityLabel: UILabel!
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
